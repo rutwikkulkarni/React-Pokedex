c5df98c2ae999521d8770ebb6bebea8190a3fde4
rutwik.kulkarni <sc18rk@leeds.ac.uk>
2021-03-29 19:42:53 +0100

    Working search and gen filter

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,50 +1,127 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Pokemon from './Pokemon'
 
 const Pokedex = () => {
   
-  const makePokedexInRange = (start, end) => {
+  const [pokedex, setPokedex] = useState([]);
+
+  const padNumber = (number, length) => {
+    let str = "" + number;
+    while(str.length < length){
+      str = "0" + str;
+    }
+    return str;
+  }
+
+  const makePokedexInRange = async (start, end) => {
     let arr = [];
     for(let i = start; i <= end; i++){
-      arr.push(i);
+      let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
+      let data = await response.json();
+      if(response.ok && data){
+        //to prevent results loading after switching gens
+        let expectedGen = document.getElementById("gen-select");
+        let expectedMinRange = 0;
+        let expectedMaxRange = 0;
+        if(expectedGen.value === "1"){
+          expectedMinRange = 1;
+          expectedMaxRange = 151;
+        }else if(expectedGen.value === "2"){
+          expectedMinRange = 152;
+          expectedMaxRange = 251;
+        }else if(expectedGen.value === "3"){
+          expectedMinRange = 252;
+          expectedMaxRange = 386;
+        }else if(expectedGen.value === "4"){
+          expectedMinRange = 387;
+          expectedMaxRange = 493;
+        }
+        let latestID = expectedMinRange - 1;
+        if(document.getElementsByClassName("pokemon-entry") !== null){
+          let arr = Array.from(document.getElementsByClassName("pokemon-entry"));
+          if(arr.length > 0){
+            latestID = arr[arr.length - 1].id;
+          }
+        }
+        if(i >= expectedMinRange && i <= expectedMaxRange && latestID == i - 1){
+          setPokedex(pokedex => [...pokedex, {
+            sprite: "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/" + padNumber(i, 3) + ".png",
+            name: data.name[0].toUpperCase() + data.name.substr(1, data.name.length),
+            number: i,
+            types: data.types,
+            enabled: true
+          }]);
+        }
+      }
     }
     return arr;
   }
-    
-  const [pokedex, setPokedex] = useState(makePokedexInRange(1, 151));
 
-  const genChange = () => {
+  const genSet = () => {
     let element = document.getElementById("gen-select");
-    console.log(element.value);
     if(element.value === "1"){
-      setPokedex(makePokedexInRange(1, 151));
+      setPokedex([]);
+      makePokedexInRange(1, 151);
     }
     else if(element.value === "2"){
-      setPokedex(makePokedexInRange(152, 251));
+      setPokedex([]);
+      makePokedexInRange(152, 251);
     }
     else if(element.value === "3"){
-      setPokedex(makePokedexInRange(252, 386));
+      setPokedex([]);
+      makePokedexInRange(252, 386);
     }
     else if(element.value === "4"){
-      setPokedex(makePokedexInRange(387, 493));
+      setPokedex([]);
+      makePokedexInRange(387, 493);
     }
   }
 
   const searchPokemon = () => {
     let searchValue = document.getElementById("pokemon-search");
     let potentialResults = document.getElementsByClassName("pokemon-name");
+    let resultArray = [];
     for(let item of potentialResults){
-      if(item.innerHTML.toString().startsWith(searchValue.value)){
-        console.log(item.innerText);
+      if(item.innerHTML.toString().toLowerCase().startsWith(searchValue.value.toLowerCase())){
+        let pokemonID = item.parentElement.getElementsByClassName("pokemon-number")[0].innerHTML;
+        console.log(item.innerHTML);
+        resultArray.push(pokemonID);
+      }
+    }
+    for(let i = 0; i < pokedex.length; i++){
+      let id = document.getElementById(pokedex[i].number);
+      let found = false;
+      for(let item of resultArray){
+        if(id.id === item.toString()){
+          id.style.display = "";
+          found = true;
+          break;
+        }
+      }
+      if(!found) {
+        id.style.display = "none";
       }
     }
   }
 
+  const checkSorted = () => {
+    for(let i = 0; i < pokedex.length - 1; i++){
+      if(pokedex[i].number > pokedex[i+1].number){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  useEffect(() => {
+    makePokedexInRange(1, 15);
+  }, []);
+  
   return(
     <div>
       <div className="pokedex-options">
         <input type="text" id="pokemon-search" name="pokemon-search" placeholder="Search" onInput={searchPokemon}></input>
-        <select name="gen-select" id="gen-select" onChange={genChange}>
+        <select name="gen-select" id="gen-select" onChange={genSet}>
           <option value="1">Gen I</option>
           <option value="2">Gen II</option>
           <option value="3">Gen III</option>
@@ -52,7 +129,7 @@ const Pokedex = () => {
         </select>
       </div>
       <div className="pokedex-container">
-        {pokedex.map((i) => <Pokemon number={i} /> )}
+        {pokedex.map((i) => <Pokemon pokemonData={i}/>)}
       </div>
     </div>
   );
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -11,46 +11,24 @@ const Pokemon = (props) => {
           name: "default"
         }
       }
-    ]
+    ],
+    enabled: true
   });
 
-  const padNumber = (number, length) => {
-    let str = "" + number;
-    while(str.length < length){
-      str = "0" + str;
-    }
-    return str;
-  }
-
-  const fetchPokemon = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + props.number)
-    .then(response => response.json())
-    .then(data => {
-      setPokemonData({
-        sprite: "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/" + padNumber(props.number, 3) + ".png",
-        name: data.name[0].toUpperCase() + data.name.substr(1, data.name.length),
-        number: props.number,
-        types: data.types
-      });
-    })
-    .catch(error => console.log(error)); 
-  }
-
   useEffect(() => {
-    console.log("tes");
-    fetchPokemon();
-  }, [props.number]);
+    setPokemonData(props.pokemonData);
+  }, []);
 
   const goToInfo = () => {
     
   }
 
   return(
-    <div className="pokemon-entry" onClick={goToInfo}>
+    <div className="pokemon-entry" onClick={goToInfo} id={props.pokemonData.number}>
       <header>
         <div className={"pokemon-entry-header type-" + pokemonData.types[0].type.name}>
-          <h2 class="pokemon-name">{pokemonData.name}</h2>
-          <h2 class="pokemon-number">{pokemonData.number}</h2>
+          <h2 className="pokemon-name">{pokemonData.name}</h2>
+          <h2 className="pokemon-number">{pokemonData.number}</h2>
         </div>
       </header>
       <div className="pokemon-entry-content">
